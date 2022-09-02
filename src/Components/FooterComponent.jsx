3046e7decdf9adefbf14b82c06717b2c8fb4a810
Giovanni Biancuzzo <giovannibiancuzzo@protonmail.com>
2022-09-02 14:22:23 +0200

    fix grafici

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -9,63 +9,56 @@ const FooterComponent = (props) => (
         className="navbar navbar-expand-lg "
         // style={{ backgroundColor: "#221f1f" }}
     >
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            {/* <Nav>
+        {/* <Nav>
                 <FontAwesomeIcon icon="fa-brands fa-instagram" />
                 <FontAwesomeIcon icon="fa-brands fa-facebook" />
                 <FontAwesomeIcon icon="fa-brands fa-twitter" />
                 <FontAwesomeIcon icon="fa-brands fa-youtube" />
             </Nav> */}
-            <Nav className="mr-auto">
-                <ListGroup>
-                    <ListGroup.Item href="#">
-                        Audio and Subtitles
-                    </ListGroup.Item>
-                    <ListGroup.Item href="#">Media Center</ListGroup.Item>
-                    <ListGroup.Item href="#">Privacy</ListGroup.Item>
-                    <ListGroup.Item href="#">Contact us</ListGroup.Item>
-                </ListGroup>
-            </Nav>
-            <Nav className="mr-auto">
-                <ListGroup>
-                    <ListGroup.Item href="#">Audio Description</ListGroup.Item>
-                    <ListGroup.Item href="#">Investor Relations</ListGroup.Item>
-                    <ListGroup.Item href="#">Legal Notices</ListGroup.Item>
-                </ListGroup>
-            </Nav>
-            <Nav className="mr-auto">
-                <ListGroup>
-                    <ListGroup.Item href="#">Help Center</ListGroup.Item>
-                    <ListGroup.Item href="#">Jobs</ListGroup.Item>
-                    <ListGroup.Item href="#">Cookie Preferences</ListGroup.Item>
-                </ListGroup>
-            </Nav>
-            <Nav className="mr-auto">
-                <ListGroup>
-                    <ListGroup.Item href="#">Gift Cards</ListGroup.Item>
-                    <ListGroup.Item href="#">Terms of Use</ListGroup.Item>
-                    <ListGroup.Item href="#">
-                        Corporate Information
-                    </ListGroup.Item>
-                </ListGroup>
-            </Nav>
-            <div className="row">
-                <div className="col col-12 text-left mb-2">
-                    <button
-                        type="button"
-                        className="btn btn-sm footer-button rounded-0 mt-3"
-                    >
-                        Service Code
-                    </button>
-                </div>
+        <Nav className="mr-auto">
+            <ListGroup>
+                <ListGroup.Item href="#">Audio and Subtitles</ListGroup.Item>
+                <ListGroup.Item href="#">Media Center</ListGroup.Item>
+                <ListGroup.Item href="#">Privacy</ListGroup.Item>
+                <ListGroup.Item href="#">Contact us</ListGroup.Item>
+            </ListGroup>
+        </Nav>
+        <Nav className="mr-auto">
+            <ListGroup>
+                <ListGroup.Item href="#">Audio Description</ListGroup.Item>
+                <ListGroup.Item href="#">Investor Relations</ListGroup.Item>
+                <ListGroup.Item href="#">Legal Notices</ListGroup.Item>
+            </ListGroup>
+        </Nav>
+        <Nav className="mr-auto">
+            <ListGroup>
+                <ListGroup.Item href="#">Help Center</ListGroup.Item>
+                <ListGroup.Item href="#">Jobs</ListGroup.Item>
+                <ListGroup.Item href="#">Cookie Preferences</ListGroup.Item>
+            </ListGroup>
+        </Nav>
+        <Nav className="mr-auto">
+            <ListGroup>
+                <ListGroup.Item href="#">Gift Cards</ListGroup.Item>
+                <ListGroup.Item href="#">Terms of Use</ListGroup.Item>
+                <ListGroup.Item href="#">Corporate Information</ListGroup.Item>
+            </ListGroup>
+        </Nav>
+        <div className="row">
+            <div className="col col-12 text-left mb-2">
+                <button
+                    type="button"
+                    className="btn btn-sm footer-button rounded-0 mt-3"
+                >
+                    Service Code
+                </button>
             </div>
-            <div className="row">
-                <div className="col col-12 text-left mb-2 mt-2 copyright">
-                    © 1997-2022 Netflix, Inc.
-                </div>
+        </div>
+        <div className="row">
+            <div className="col col-12 text-left mb-2 mt-2 copyright">
+                © 1997-2022 Netflix, Inc.
             </div>
-        </Navbar.Collapse>
+        </div>
     </Navbar>
 );
 
