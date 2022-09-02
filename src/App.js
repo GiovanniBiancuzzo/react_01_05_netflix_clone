cde202035bba32abf1c5f22d0af237d906b65c72
Giovanni Biancuzzo <giovannibiancuzzo@protonmail.com>
2022-09-02 17:15:24 +0200

    chiamate ottimizzate

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import NavbarComponent from './Components/NavbarComponent';
 import TitlebarComponent from './Components/TitlebarComponent';
 import MoviesList from './Components/MoviesList';
 import FooterComponent from './Components/FooterComponent';
+import { Container, Row } from 'react-bootstrap';
 
 function App () {
   return (
     <div className="App">
       <NavbarComponent />
       <TitlebarComponent />
-      <MoviesList />
+      <Container fluid>
+        <Row>
+          <h3>Star Wars</h3>
+        </Row>
+        <MoviesList movieSearch="star%20wars" />
+        <Row>
+          <h3>Harry Potter</h3>
+        </Row>
+        <MoviesList movieSearch="harry%20potter" />
+        <Row>
+          <h3>Avengers</h3>
+        </Row>
+        <MoviesList movieSearch="avengers" />
+      </Container>
       <FooterComponent />
     </div>
   );
--- src/Components/MoviesList.jsx
+++ src/Components/MoviesList.jsx
@@ -4,96 +4,40 @@ import Movie from "./Movie";
 
 class MoviesList extends Component {
     state = {
-        movies1: [],
-        movies2: [],
-        movies3: [],
+        movies: [],
     };
 
-    fetchMovies1 = (saga) => {
+    fetchMovies = () => {
         console.log("Ottenimento dei film");
         // fetch("http://www.omdbapi.com/?apikey=9570e6e6&s=harry%20potter");
-        fetch(`http://www.omdbapi.com/?apikey=9570e6e6&s=${saga}`, {
-            method: "GET",
-        })
+        fetch(
+            `http://www.omdbapi.com/?apikey=9570e6e6&s=${this.props.movieSearch}`,
+            {
+                method: "GET",
+            }
+        )
             .then((res) => res.json())
             .then((data) => {
                 this.setState({
-                    movies1: data.Search,
-                });
-            })
-            .catch((error) => console.log("Qualcosa è andato storto", error));
-    };
-
-    fetchMovies2 = (saga) => {
-        console.log("Ottenimento dei film");
-        // fetch("http://www.omdbapi.com/?apikey=9570e6e6&s=harry%20potter");
-        fetch(`http://www.omdbapi.com/?apikey=9570e6e6&s=${saga}`, {
-            method: "GET",
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                this.setState({
-                    movies2: data.Search,
-                });
-            })
-            .catch((error) => console.log("Qualcosa è andato storto", error));
-    };
-
-    fetchMovies3 = (saga) => {
-        console.log("Ottenimento dei film");
-        // fetch("http://www.omdbapi.com/?apikey=9570e6e6&s=harry%20potter");
-        fetch(`http://www.omdbapi.com/?apikey=9570e6e6&s=${saga}`, {
-            method: "GET",
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                this.setState({
-                    movies3: data.Search,
+                    movies: data.Search,
                 });
             })
             .catch((error) => console.log("Qualcosa è andato storto", error));
     };
 
     componentDidMount() {
-        this.fetchMovies1("harry%20potter");
-        this.fetchMovies2("star%20wars");
-        this.fetchMovies3("avengers");
+        this.fetchMovies();
     }
 
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <h3>Trending Now</h3>
-                </Row>
-                <Row>
-                    {this.state.movies1.map((movie) => (
-                        <Col key={movie.imdbID} xs={6} md={4} lg={2}>
-                            <Movie movieItem={movie} />
-                        </Col>
-                    ))}
-                </Row>
-                <Row>
-                    <h3>Watch it again</h3>
-                </Row>
-                <Row>
-                    {this.state.movies2.map((movie) => (
-                        <Col key={movie.imdbID} xs={6} md={4} lg={2}>
-                            <Movie movieItem={movie} />
-                        </Col>
-                    ))}
-                </Row>
-                <Row>
-                    <h3>New Releases</h3>
-                </Row>
-                <Row>
-                    {this.state.movies3.map((movie) => (
-                        <Col key={movie.imdbID} xs={6} md={4} lg={2}>
-                            <Movie movieItem={movie} />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+            <Row>
+                {this.state.movies.map((movie) => (
+                    <Col key={movie.imdbID} xs={6} md={4} lg={2}>
+                        <Movie movieItem={movie} />
+                    </Col>
+                ))}
+            </Row>
         );
     }
 }
