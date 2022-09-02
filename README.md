b0bc2453cd119857fce4d9b55b8ae24c838b9812
Giovanni Biancuzzo <giovannibiancuzzo@protonmail.com>
2022-09-02 17:23:41 +0200

    Figure + Accordion

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+graficamente ho utilizzato Figure + Accordion di react bootstrap
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,20 +1,29 @@
 import { Component } from "react";
-import { Figure } from "react-bootstrap";
+import { Accordion, Figure } from "react-bootstrap";
 
 class Movie extends Component {
     state = {};
     //prop si chiama movieItem
     render() {
         return (
-            <Figure>
-                <Figure.Image
-                    width={171}
-                    height={180}
-                    alt={this.props.movieItem.imbdID}
-                    src={this.props.movieItem.Poster}
-                />
-                <Figure.Caption>{this.props.movieItem.Title}</Figure.Caption>
-            </Figure>
+            <Accordion defaultActiveKey="1">
+                <Figure>
+                    <Accordion.Toggle eventKey="0">
+                        <Figure.Image
+                            width={171}
+                            height={180}
+                            alt={this.props.movieItem.imbdID}
+                            src={this.props.movieItem.Poster}
+                        />
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0">
+                        <Figure.Caption>
+                            {this.props.movieItem.Title}
+                        </Figure.Caption>
+                    </Accordion.Collapse>
+                </Figure>
+            </Accordion>
         );
     }
 }
